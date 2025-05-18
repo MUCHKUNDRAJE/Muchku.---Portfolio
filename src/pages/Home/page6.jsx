b533bb3aa91fd64a59aa7c5f36171377fd9326b0
MUCHKUNDRAJE <muchkundraje.thote@vectorlab.in>
2025-05-18 12:05:59 +0530

    changes

--- src/pages/Home/page6.jsx
+++ src/pages/Home/page6.jsx
@@ -1,10 +1,6 @@
 import React, { useRef } from "react";
 // import gsap from "gsap";
 // import { useGSAP } from "@gsap/react";
-import Card from "../../components/resuable/Card";
-import JourneyCard from "../../components/resuable/JourneyCard";
-import Journeydata from "../../database/Journeydata";
-import { Button } from "../../components/ui/button";
 import gsap from "gsap";
 // import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger"; 
